34c4bb65c0141b66cdff3d0ed9c095219a51aa19
Sam Bozman <s.j.bozman@gmail.com>
2022-11-29 11:06:08 -0800

    Set up hard coded directories for Max_Positions and Target data

--- src/setup.h
+++ src/setup.h
@@ -3,16 +3,23 @@
 
 void setup()
 {
+
     Serial.begin(115200);
 
     mountLFS(); //* Return message to app if failed?
     ESP_BT.begin("ESP32_Control"); // Name of your Bluetooth interface
-    if (!isDir(LittleFS, "/MAX", 1)) // used to store stepper max positions
+    delay(5000);
+    // directory used to store stepper max positions
+    if (!isDir(LittleFS, "/MAX", 1))
         createDir(LittleFS, "/MAX");
 
-    homeStepper(Hstepper, hHomePin);
-    // homeStepper(Vstepper, vHomePin);
-    // homeStepper(Sstepper, sHomePin);
+    // directoryused to Target data
+    if (!isDir(LittleFS, "/TARGETS", 1))
+        createDir(LittleFS, "/TARGETS");
+
+    // homeStepper(Hstepper, hHomePin);
+    //  homeStepper(Vstepper, vHomePin);
+    //  homeStepper(Sstepper, sHomePin);
 
     // Shutoff water
     // TODO start a timer to shut down steppers after a specific time period.
